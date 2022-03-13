d49dd050e892aa9098a537e0c00e306dc7b1eab6
Near Huscarl <near.huscarl@gmail.com>
2022-03-13 13:22:44 +0700

    refine guass gun

--- src/BotExtended/Projectiles/GaussGun.cs
+++ src/BotExtended/Projectiles/GaussGun.cs
@@ -9,7 +9,7 @@ using static BotExtended.Library.SFD;
 
 namespace BotExtended.Projectiles
 {
-    class GaussGun: RangeWpn
+    class GaussGun : RangeWpn
     {
         public float ChargeModifier { get; private set; }
 
@@ -19,9 +19,7 @@ namespace BotExtended.Projectiles
             return !Projectile.IsSlowProjectile(Mapper.GetProjectile(Name));
         }
 
-        public GaussGun(IPlayer owner, WeaponItem name) : base(owner, name, RangedWeaponPowerup.Gauss)
-        {
-        }
+        public GaussGun(IPlayer owner, WeaponItem name) : base(owner, name, RangedWeaponPowerup.Gauss) { }
 
         private bool m_prevManualAiming = false;
         public override void Update(float elapsed)
@@ -52,19 +50,16 @@ namespace BotExtended.Projectiles
 
             var start = projectile.Position;
             var end = start + projectile.Direction * MaxRange;
-            var maxHitCount = (int)(ChargeModifier / 1000 + 1);
+            var maxHitCount = Game.IsEditorTest ? 5000 : (int)(ChargeModifier / 1000 + 1);
             var results = Game.RayCast(start, end, new RayCastInput()
             {
                 ProjectileHit = RayCastFilterMode.True,
                 IncludeOverlap = true,
                 ClosestHitOnly = maxHitCount == 1,
             }).Where(r => r.HitObject != null);
+            var props = projectile.GetProperties();
 
-            end = results.Count() == 0 ? end : results.First().Position;
-
-            var distance = Vector2.Distance(start, end);
-            for (var i = 0f; i <= distance; i+=1.5f)
-                Game.PlayEffect(EffectName.ItemGleam, start + projectile.Direction * i);
+            end = results.Count() == 0 ? end : results.Last().Position;
 
             var hitCount = 0;
             foreach (var result in results)
@@ -73,29 +68,23 @@ namespace BotExtended.Projectiles
                 var projectileItem = projectile.ProjectileItem;
                 var direction = projectile.Direction;
                 var powerup = ScriptHelper.GetPowerup(projectile);
+                var cf = hitObject.GetCollisionFilter();
 
                 Game.PlayEffect(EffectName.Electric, result.Position);
                 Game.PlaySound("ElectricSparks", result.Position);
-                ScriptHelper.Timeout(() =>
+
+                if (cf.AbsorbProjectile)
                 {
-                    var p = Game.SpawnProjectile(projectileItem, result.Position, direction, powerup);
-                    p.CritChanceDealtModifier *= 3;
-                    if (!hitObject.GetCollisionFilter().AbsorbProjectile)
-                        ScriptHelper.Timeout(() => p.FlagForRemoval(), 0);
-                }, 0);
-                //if (Game.IsEditorTest)
-                //{
-                //    ScriptHelper.RunIn(() =>
-                //    {
-                //        if (hitObject != null)
-                //            Game.DrawArea(hitObject.GetAABB());
-                //        Game.DrawLine(start, end, Color.Cyan);
-                //    }, 500);
-                //}
-                if (hitObject.GetCollisionFilter().AbsorbProjectile)
                     hitCount++;
+                    if (cf.CategoryBits == CategoryBits.StaticGround) { end = result.Position; break; }
+                }
+                hitObject.DealDamage(result.IsPlayer ? props.PlayerDamage : props.ObjectDamage);
                 if (hitCount >= maxHitCount) break;
             }
+
+            var distance = Vector2.Distance(start, end);
+            for (var i = 0f; i <= distance; i += 1.5f)
+                Game.PlayEffect(EffectName.ItemGleam, start + projectile.Direction * i);
 
             projectile.FlagForRemoval();
         }
